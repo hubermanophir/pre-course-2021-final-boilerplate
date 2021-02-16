c22d407387af0cb3e75f9b77be54a6350923f23e
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-02-16 19:43:14 +0200

    delete specific item works

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,46 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
-const todoItems = require('./todoItems.json');
-
+const todoItems = require("./todoItems.json");
 
 //gets all the tasks
-app.get('/b', (req, res) => {
-	res.send(todoItems);
+app.get("/b", (req, res) => {
+  res.send(todoItems);
 });
 
 //gets a specific task
-app.get('/b/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-    res.send(todoItems.filter(item => item.id === id));
+app.get("/b/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send(todoItems.filter((item) => item.id === id));
 });
 
-//pushes a json 
-app.post('/b', (req, res) => {
-    todoItems.push(req.body);
-    res.send(req.body);
+//pushes a json
+app.post("/b", (req, res) => {
+  todoItems.push(req.body);
+  res.send(req.body);
 });
-
 
 //updates an object
-app.put('/b/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    for (let i = 0; i < todoItems.length; i++) {
-        if (todoItems[i].id === id) {
-            todoItems[i] = req.body;
-            res.send(req.body);
-        } 
+app.put("/b/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id === id) {
+      todoItems[i] = req.body;
+      res.send(req.body);
     }
+  }
 });
 
-app.delete('/b/:id', (req, res) => {
-    const id = req.params.id;
-    for (let i = 0; i < todoItems.length; i++) {
-        if (todoItems[i].id === id) {
-            todoItems.splice(i,1);
-            res.send('removed');
-        } 
+app.delete("/b/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id === id) {
+      todoItems.splice(i, 1);
+      res.send("removed");
     }
+  }
 });
 
 app.listen(3000);
