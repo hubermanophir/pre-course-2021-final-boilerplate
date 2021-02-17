80530f7bf1f507c77d6fc4218f305cf9ff5f6c6d
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-02-17 14:27:57 +0200

    made a create entry

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,79 +1,21 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = 3000;
-// const todoItems = require("./b.json");
+const PORT = 3000;
 const fs = require("fs");
-const path = require('path');
-app.listen(port);
 
-const getAllDirFiles = function(dirPath, arrayOfFiles) {
-  files = fs.readdirSync(dirPath)
-
-  arrayOfFiles = arrayOfFiles || []
-
-  files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllDirFiles(dirPath + "/" + file, arrayOfFiles)
-    } else {
-      arrayOfFiles.push(file)
-    }
-  })
-
-  return arrayOfFiles
-}
-
-
-// console.log(getAllDirFiles('./b').length);
-//gets all the tasks
-app.get("/b", (req, res) => {
-  res.send();
+//create file with the corresponding id as name
+app.post("/b", (req, res) => {
+  const { body } = req;
+  const { id } = body;
+  fs.writeFileSync(`./database/${id}.json`, JSON.stringify(body, null, 4));
+  res.send(req.body);
 });
 
-
-app.get("/b/:collectionName/:jsonName", (req, res) => {
-  fs.readFile(`b/${req.params.collectionName}/${req.params.jsonName}.json`, 'utf8', (err,data) => { 
-    if (err) {
-          res.send(err);
-      } else {
-          res.send(data);
-      }
-  })
-    
-});
-
-//gets a specific task
+//get file with using the id
 app.get("/b/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  res.send(todoItems.filter((item) => item.id === id));
+  res.json(fs.readFileSync(`./database/${req.params.id}.json`));
 });
 
-// //pushes a json
-// app.post("/b", (req, res) => {
-//   todoItems.push(req.body);
-//   fs.writeFileSync('./todoItems.json', JSON.stringify(todoItems));
-//   res.send(req.body);
-// });
-
-// //updates an object
-// app.put("/b/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   for (let i = 0; i < todoItems.length; i++) {
-//     if (todoItems[i].id === id) {
-//       todoItems[i] = req.body;
-//       res.send(req.body);
-//     }
-//   }
-// });
-
-// //deletes an item using id
-// app.delete("/b/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   for (let i = 0; i < todoItems.length; i++) {
-//     if (todoItems[i].id === id) {
-//       todoItems.splice(i, 1);
-//       res.send("removed");
-//     }
-//   }
-// });
-
+app.listen(PORT);
+console.log(`listening on port: ${PORT}`);
